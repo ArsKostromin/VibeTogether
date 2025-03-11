1c2834a0ab89789f32ec56df70a1f21431444d40
ArsKostromin <arsyakost@gmail.com>
2025-03-11 22:02:50 +0500

    model event

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Events
 
 # Register your models here.
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'published', 'location', 'img')
+    list_display_links = ('name', 'description')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {"slug": ("name", )}
+    
+admin.site.register(Events, EventsAdmin)
--- events/models.py
+++ events/models.py
@@ -5,10 +5,15 @@ class Events(models.Model):
     name = models.CharField(max_length=35, verbose_name='название мероприятия')
     img = models.ImageField(upload_to='images', null=True, blank=True, verbose_name='Картинка')
     description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    location = models.CharField(max_length=35, null=True, blank=True, verbose_name='место проведения')
     published = models.DateField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
-    status = 
+    vote = models.IntegerField(default=0, null=True, blank=True, verbose_name='голоса')
+    date = models.DateField(null=True, blank=True, verbose_name='дата проведения')
     slug = models.SlugField(unique=True, verbose_name="URL", db_index=True)
 
-    
+    class Meta:
+        verbose_name_plural = 'Мероприятия'
+        verbose_name = 'мероприятие'
+        ordering = ['-published']
     
     
